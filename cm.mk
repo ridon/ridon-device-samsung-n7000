eb0be7cb6b65730b4352a87570710cc202026285
ahmad haris <princeofgiri@di.blankon.in>
2014-11-22 13:30:25 +0700

    branding from replicant to ridon

--- cm.mk
+++ cm.mk
@@ -15,20 +15,20 @@
 #
 
 # Specify phone tech before including full_phone
-$(call inherit-product, vendor/replicant/config/gsm.mk)
+$(call inherit-product, vendor/ridon/config/gsm.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := GT-N7000
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/replicant/config/common_full_phone.mk)
+$(call inherit-product, vendor/ridon/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/n7000/full_n7000.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := n7000
-PRODUCT_NAME := replicant_n7000
+PRODUCT_NAME := ridon_n7000
 PRODUCT_BRAND := Samsung
 PRODUCT_MODEL := GT-N7000
 
